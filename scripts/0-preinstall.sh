d8f3e847b76ac9bb257a9a76d1e5428905762dac
Max <maxigor.ferreira@gmail.com>
2023-04-18 22:16:18 -0300

    no more fastest mirror

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -27,13 +27,7 @@ pacman -S --noconfirm --needed pacman-contrib
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 pacman -S --noconfirm --needed reflector rsync grub
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-echo -ne "
--------------------------------------------------------------------------
-                    Setting up $iso mirrors for faster downloads
--------------------------------------------------------------------------
-"
-reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-mkdir /mnt &>/dev/null # Hiding error message if any
+
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Prerequisites
