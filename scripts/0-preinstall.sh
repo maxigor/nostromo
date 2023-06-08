bd5650af7f59bca3b3b8b453c9190f0ca6943d39
Max <maxigor.ferreira@gmail.com>
2023-06-08 07:26:42 -0300

    linux lts

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -138,7 +138,7 @@ echo -ne "
                     Arch Install on Main Drive
 -------------------------------------------------------------------------
 "
-pacstrap /mnt base base-devel linux linux-firmware vim sudo archlinux-keyring wget libnewt --noconfirm --needed
+pacstrap /mnt base base-devel linux-lts linux-firmware vim sudo archlinux-keyring wget libnewt --noconfirm --needed
 echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R ${SCRIPT_DIR} /mnt/root/nostromo
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
