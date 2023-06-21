705dd077fefc212dc9dbc494d6db21992cdd78ed
Max <maxigor.ferreira@gmail.com>
2023-06-21 19:11:19 -0300

    xautolock new cfg

--- configs/.config/bspwm/autostart.sh
+++ configs/.config/bspwm/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 xbacklight -set 20 &
+xautolock -time 5 -locker i3lock-fancy &
 polybar &
 cbatticon &
 notion-app &
 thunderbird &
 firefox &
 alacritty &
-xautolock -time 5 -locker i3lock-fancy &
