f58bfd859dc3a446f01cff1d3a4478bd64336cc6
Max <maxigor.ferreira@gmail.com>
2024-02-07 15:32:05 -0300

    autolock & xset 10 minutes to lock

--- configs/.config/bspwm/autostart.sh
+++ configs/.config/bspwm/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 xbacklight -set 20 &
-xautolock -time 5 -locker i3lock-fancy &
-xset s 360 360
+xautolock -time 10 -locker i3lock-fancy &
+xset dpms 600 600 600
 polybar &
 nordvpn c &
 thunderbird &
